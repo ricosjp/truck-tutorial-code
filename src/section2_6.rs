044fb8425bbff1dbc82e2e7bdabf084f12756353
Yoshinori Tanimura <tanimura@ricos.co.jp>
2021-02-05 01:29:32 +0900

    add comments to section2_6

--- src/section2_6.rs
+++ src/section2_6.rs
@@ -136,79 +136,98 @@ impl App for MyApp {
     fn render(&mut self, frame: &SwapChainFrame) { self.scene.render_scene(&frame.output.view) }
 }
 
+// modeling the body shape
+// # Arguments
+// - bottom: y-coordinate of the bottom face
+// - height: height of the body
+// - width: width of the body
+// - thickness: thickness of the body
 fn body_shell(bottom: f64, height: f64, width: f64, thickness: f64) -> Shell {
+    // draw a circle arc
     let vertex0 = builder::vertex(Point3::new(-width / 2.0, bottom, thickness / 4.0));
     let vertex1 = builder::vertex(Point3::new(width / 2.0, bottom, thickness / 4.0));
     let transit = Point3::new(0.0, bottom, thickness / 2.0);
     let arc0 = builder::circle_arc(&vertex0, &vertex1, transit);
+    // copy and rotate the circle arc
     let arc1 = builder::rotated(&arc0, Point3::origin(), Vector3::unit_y(), Rad(PI));
+    // create the homotopy from arc0 to arc1.inverse()
     let face = builder::homotopy(&arc0, &arc1.inverse());
+    // create the body
     let solid = builder::tsweep(&face, Vector3::new(0.0, height, 0.0));
+    // Return the solid as a boundary shell for easier processing later.
     solid.into_boundaries().pop().unwrap()
 }
 
+// modeling a cylinder
+// # Arguments
+// - bottom: y-coordinate of the bottom disk
+// - height: height of the cylinder
+// - radius: radius of the bottom disk
 fn cylinder(bottom: f64, height: f64, radius: f64) -> Shell {
+    // make a solid cylinder
     let vertex = builder::vertex(Point3::new(0.0, bottom, radius));
     let circle = builder::rsweep(&vertex, Point3::origin(), Vector3::unit_y(), Rad(7.0));
     let disk = builder::try_attach_plane(&vec![circle]).unwrap();
     let solid = builder::tsweep(&disk, Vector3::new(0.0, height, 0.0));
+    // Return the solid as a boundary shell for easier processing later.
     solid.into_boundaries().pop().unwrap()
 }
 
+// sew the body and the neck
 fn grue_body_neck(body: &mut Shell, neck: Shell) {
+    // get the body's seiling
     let body_seiling = body.last_mut().unwrap();
+    // the boundary of the neck's bottom
     let wire = neck[0].boundaries()[0].clone();
+    // drill a hole in the body using the boundary of the neck's bottom
     body_seiling.add_boundary(wire);
+    // add the faces of the neck to the body other than the bottom
     body.extend(neck.into_iter().skip(1));
 }
-/*
+
 // modeling a bottle
 fn bottle(height: f64, width: f64, thickness: f64) -> Solid {
-    let mut body = body_shell(0.0, height, width, thickness);
-    let neck = cylinder(height, height / 10.0, thickness / 4.0);
+    // create the body of the bottle
+    let mut body = body_shell(-height / 2.0, height, width, thickness);
+    // create the neck of the bottle
+    let neck = cylinder(height / 2.0, height / 10.0, thickness / 4.0);
+    // sew the body and the neck
     grue_body_neck(&mut body, neck);
 
+    // distance between outer and inner surface, i.e. the thickness of the faces.
     let eps = height / 50.0;
+    // inner body. Make it small enough to account for thickness.
     let mut inner_body = body_shell(
-        eps,
+        -height / 2.0 + eps,
         height - 2.0 * eps,
         width - 2.0 * eps,
         thickness - 2.0 * eps,
     );
-    let inner_neck = cylinder(height - eps, height / 10.0 + eps, thickness / 4.0 - eps);
+    // inner neck. Make it long and narrow to account for thickness.
+    let inner_neck = cylinder(
+        height / 2.0 - eps,
+        height / 10.0 + eps,
+        thickness / 4.0 - eps,
+    );
+    // sew the inner body and the inner neck
     grue_body_neck(&mut inner_body, inner_neck);
 
-    let inner_hat = inner_body.pop().unwrap();
-    let wire = inner_hat.into_boundaries()[0].inverse();
-    body.last_mut().unwrap().add_boundary(wire);
-    body.extend(inner_body.into_iter().map(|face| face.inverse()));
-
-    builder::translated(
-        &Solid::new(vec![body]),
-        Vector3::new(0.0, -height / 2.0, 0.0),
-    )
-}
-*/
-
-fn bottle(_height: f64, _width: f64, _thickness: f64) -> Shell {
-    // 立方体を作成する
-    let vertex: Vertex = builder::vertex(Point3::new(-0.5, -0.5, -0.5));
-    let edge: Edge = builder::tsweep(&vertex, Vector3::unit_z());
-    let face: Face = builder::tsweep(&edge, Vector3::unit_x());
-    let cube: Solid = builder::tsweep(&face, Vector3::unit_y());
-    
-    // 立方体をシェルにする
-    let mut shell: Shell = cube.into_boundaries().pop().unwrap();
-    // 底面に加える穴を開けるための境界を作る
-    let circle: Wire = builder::rsweep(
-        &builder::vertex(Point3::new(0.0, -0.5, 0.25)),
-        Point3::new(0.0, -0.5, 0.0),
-        Vector3::unit_y(),
-        Rad(7.0),
-    );
-    // 底面に境界を加えて穴をあける
-    shell[0].add_boundary(circle);
-    shell
+    // invert all faces of the inner body
+    inner_body.face_iter_mut().for_each(|face| {
+        face.invert();
+    });
+    // pop the seiling of the inner body
+    let inner_seiling = inner_body.pop().unwrap();
+    // make the inner seiling the boundary wire
+    let wire = inner_seiling.into_boundaries().pop().unwrap();
+    // the mutable reference of the outer seiling
+    let seiling = body.last_mut().unwrap();
+    // drill a hole in the outer seiling using the boundary of inner seiling
+    seiling.add_boundary(wire);
+    // add the faces of the neck to the body
+    body.extend(inner_body.into_iter());
+    // returns the solid
+    Solid::new(vec![body])
 }
 
 // Run!
